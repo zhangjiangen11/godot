d858ce4eabca66e64ec46d14fb767375ce636da4
zhangjiangen11 <279878713@qq.com>
2025-04-04 04:59:35 +0800

    修改格式错误

--- modules/game_help/PathMesh3D/register_types.cpp
+++ modules/game_help/PathMesh3D/register_types.cpp
@@ -25,20 +25,3 @@ void initialize_path_mesh_3d(ModuleInitializationLevel p_level) {
 void uninitialize_path_mesh_3d(ModuleInitializationLevel p_level) {
 	// void
 }
-
-// extern "C" {
-//     // Initialization.
-//     GDExtensionBool GDE_EXPORT path_mesh_3d_init(
-//             GDExtensionInterfaceGetProcAddress p_get_proc_address,
-//             const GDExtensionClassLibraryPtr p_library,
-//             GDExtensionInitialization *r_initialization) {
-//         godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-
-//         init_obj.register_initializer(initialize_path_mesh_3d);
-//         init_obj.register_terminator(uninitialize_path_mesh_3d);
-//         init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-//         return init_obj.init();
-//     }
-
-// }
