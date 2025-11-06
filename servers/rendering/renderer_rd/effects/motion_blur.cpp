432c687e3d699a2eb046d76802e8130c2266a445
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2025-11-06 14:07:12 +0800

    Create motion blur source and shaders

---
+++ servers/rendering/renderer_rd/effects/motion_blur.cpp
@@ -0,0 +1,1 @@
+﻿#include "motion_blur.h"
---
+++ servers/rendering/renderer_rd/effects/motion_blur.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "servers/rendering/renderer_rd/pipeline_cache_rd.h"
+#include "servers/rendering/renderer_rd/pipeline_deferred_rd.h"
+#include "servers/rendering/renderer_rd/shaders/effects/motion_blur_blur.glsl.gen.h"
+
+class MotionBlur {
+public:
+	enum MotionBlurMode {
+		MOTION_BLUR_PREPROCESS,
+		MOTION_BLUR_STANDARD,
+		MOTION_BLUR_BOKEH,
+		MOTION_BLUR_MAX,
+	};
+
+	struct {
+		MotionBlurBlurShaderRD blur_shader;
+		RID shader_version;
+		PipelineDeferredRD compute_pipelines[MOTION_BLUR_MAX];
+	} shaders;
+
+};
