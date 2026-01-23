4faffeaf7996cb3c978f28f584f0b24e8ddf85ee
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2026-01-22 19:28:07 -0500

    Update editor shortcuts when changing 3D navigation scheme

--- editor/settings/editor_settings_dialog.cpp
+++ editor/settings/editor_settings_dialog.cpp
@@ -82,6 +82,7 @@ void EditorSettingsDialog::_settings_property_edited(const String &p_name) {
 		EditorSettings::get_singleton()->set_manually("editors/3d/navigation/navigation_scheme", (int)Node3DEditorViewport::NAVIGATION_CUSTOM);
 	} else if (full_name == "editors/3d/navigation/navigation_scheme") {
 		update_navigation_preset();
+		_update_shortcuts();
 	}
 }
 
